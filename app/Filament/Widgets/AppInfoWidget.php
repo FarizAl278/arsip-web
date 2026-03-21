986889bc04da9d0769a0b57e96ad9ea7cf6abeb2
Ahmad Lesmana <ahmadlesmana788@gmail.com>
2026-03-22 00:24:57 +0700

    feat: widget info app

---
+++ app/Filament/Widgets/AppInfoWidget.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class AppInfoWidget extends Widget
+{
+
+    protected static ?int $sort = -2;
+
+    protected static bool $isLazy = false;
+    protected string $view = 'filament.widgets.app-info-widget';
+}
